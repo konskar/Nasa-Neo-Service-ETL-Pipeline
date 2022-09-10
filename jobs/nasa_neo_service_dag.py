cd2e163c5080b278048b0b2a118ffced47c94d4d
konskar <kostaskar93@gmail.com>
2022-09-10 15:37:20 +0300

    nasa_neo_service_dag.py: add send_run_success_notification task

--- jobs/nasa_neo_service_dag.py
+++ jobs/nasa_neo_service_dag.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 # Third party imports
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.email import EmailOperator
 
 # Local application imports
 from nasa_neo_service_etl_dag.jobs import functions as f
@@ -14,11 +15,11 @@ default_args = {
     "owner": "airflow",
     "depends_on_past": False,
     "start_date": datetime(2019, 4, 30),
-    "email": cfg.airflow["email_list"],
+    "email": ["kokargag@gmail.com", "konskar93@gmail.com"],
     "email_on_failure": True,
     "email_on_retry": False,
     "retries": 3,
-    "retry_delay": timedelta(minutes=2),
+    "retry_delay": timedelta(minutes=1),
 }
 
 with DAG(
@@ -27,6 +28,7 @@ with DAG(
     default_args=default_args,
     catchup=False,
 ) as dag:
+
     get_nasa_feed_api_data = PythonOperator(
         task_id="get_nasa_feed_api_data",
         python_callable=f.get_nasa_feed_api_data,
@@ -50,5 +52,11 @@ with DAG(
         python_callable=f.update_mongodb_production,
         retries=3
     )
+    send_run_success_notification = EmailOperator(
+        task_id ='send_run_success_notification',
+        to = ["kokargag@gmail.com", "konskar93@gmail.com"],
+        subject = 'Airflow Notification: "nasa_neo_service_ingestion_dag"',
+        html_content = """ <h3>Dag run sucesfully</h3> """
+    )
 
-get_nasa_feed_api_data >> make_nasa_feed_parquet_file >> load_parquet_to_mongodb_stage >> update_mongodb_production
+get_nasa_feed_api_data >> make_nasa_feed_parquet_file >> load_parquet_to_mongodb_stage >> update_mongodb_production >> send_run_success_notification
