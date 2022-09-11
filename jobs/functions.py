327031c1e53cf802f7d719e1fb3c8adffd928110
konskar <kostaskar93@gmail.com>
2022-09-11 06:42:09 +0300

    (feat): Force dag task fail if an exception is raised

--- jobs/functions.py
+++ jobs/functions.py
@@ -6,12 +6,13 @@ import json
 # Third party imports
 import requests
 import requests_cache
-
 from pyspark.sql import SparkSession
 from pyspark.sql.window import Window
 from pyspark.sql.functions import row_number
-
 from pymongo import MongoClient
+import smtplib
+import ssl
+from airflow import AirflowException
 
 # Local application imports
 from nasa_neo_service_etl_dag.configs import etl_config as cfg
@@ -41,111 +42,106 @@ def validate_date_ranges(start_date, end_date):
         raise Exception("enddate should be bigger than startdate")
 
 
-def get_nasa_feed_api_data(**kwargs):
+def collect_api_data(**kwargs):
     """
     get api data from https://api.nasa.gov/neo/rest/v1/feed?start_date=2022-03-08&end_date=2022-03-09&api_key=DEMO_KEY
     for last 4 days
     """
 
-    execution_date = kwargs['ds']
+    try:
+        execution_date = kwargs['ds']
 
-    # convert str to date object to calculate dynamic past dates, then convert again to str to pass to application
-    execution_date_object = datetime.strptime(execution_date, "%Y-%m-%d")
+        # convert str to date object to calculate dynamic past dates, then convert again to str to pass to application
+        execution_date_object = datetime.strptime(execution_date, "%Y-%m-%d")
 
-    start_date_object = execution_date_object - timedelta(days=1)
-    start_date = start_date_object.strftime("%Y-%m-%d")
+        start_date_object = execution_date_object - timedelta(days=1)
+        start_date = start_date_object.strftime("%Y-%m-%d")
 
-    end_date_object = execution_date_object - timedelta(days=0)
-    end_date = end_date_object.strftime("%Y-%m-%d")
+        end_date_object = execution_date_object - timedelta(days=0)
+        end_date = end_date_object.strftime("%Y-%m-%d")
 
-    requests_cache.install_cache(cfg.absolute_paths["cache_abs_path"])
+        requests_cache.install_cache(cfg.absolute_paths["cache_abs_path"])
 
-    validate_date_format(start_date)
-    validate_date_format(end_date)
+        validate_date_format(start_date)
+        validate_date_format(end_date)
 
-    validate_date_ranges(start_date, end_date)
+        validate_date_ranges(start_date, end_date)
 
-    url = cfg.nasa_feed_api["url"]
+        url = cfg.nasa_feed_api["url"]
 
-    params = {
-        "start_date": start_date,
-        "end_date": end_date,
-        # "start_date": '2022-07-20',
-        # "end_date": '2022-07-23',
-        "api_key": cfg.nasa_feed_api["api_key"]
-    }
+        params = {
+            "start_date": start_date,
+            "end_date": end_date,
+            # "start_date": '2022-07-20',
+            # "end_date": '2022-07-23',
+            "api_key": cfg.nasa_feed_api["api_key"]
+        }
 
-    res = requests.get(url=url, params=params, timeout=120)
-    data = res.json()
+        res = requests.get(url=url, params=params, timeout=120)
+        data = res.json()
 
-    dict_list = []
+        dict_list = []
 
-    for key, value in data["near_earth_objects"].items():
+        for key, value in data["near_earth_objects"].items():
 
-        date = key
+            date = key
 
-        for row in value:
-            neo_reference_id = row["neo_reference_id"]
-            name = row["name"]
-            nasa_jpl_url = row["nasa_jpl_url"]
-            estimated_diameter_km_min = row["estimated_diameter"]["kilometers"]["estimated_diameter_min"]
+            for row in value:
+                neo_reference_id = row["neo_reference_id"]
+                name = row["name"]
+                nasa_jpl_url = row["nasa_jpl_url"]
+                estimated_diameter_km_min = row["estimated_diameter"]["kilometers"]["estimated_diameter_min"]
 
-            _dict = {
-                'date': date,
-                'neo_reference_id': neo_reference_id,
-                'name': name,
-                'nasa_jpl_url': nasa_jpl_url,
-                'estimated_diameter_km_min': estimated_diameter_km_min,
-            }
+                _dict = {
+                    'date': date,
+                    'neo_reference_id': neo_reference_id,
+                    'name': name,
+                    'nasa_jpl_url': nasa_jpl_url,
+                    'estimated_diameter_km_min': estimated_diameter_km_min,
+                }
 
-            dict_list.append(_dict)
+                dict_list.append(_dict)
 
-    # print(json.dumps(dict_list, indent=2))
+        # print(json.dumps(dict_list, indent=2))
 
-    with open(cfg.absolute_paths["json_abs_path"], 'w') as file:
-        json.dump(dict_list, file, indent=4)
+        with open(cfg.absolute_paths["json_abs_path"], 'w') as file:
+            json.dump(dict_list, file, indent=4)
 
+    except Exception as e:
 
-def make_nasa_feed_parquet_file():
-    global spark
-
-    df_nasa_feed = spark.read.option("multiline", "true").json(
-        cfg.absolute_paths["json_abs_path"])
-
-    # df_nasa_feed.printSchema()
-    # df_nasa_feed.show()
-
-    cached_df_nasa_feed = df_nasa_feed.cache()
-
-    # ------------ dataframe api method ------------
-
-    windowSpec = Window.partitionBy("date").orderBy("neo_reference_id")
-
-    df_nasa_feed_updated_dfapi = cached_df_nasa_feed.withColumn(
-        "row_number", row_number().over(windowSpec))
-
-    # df_nasa_feed_updated_dfapi.printSchema()
-    # df_nasa_feed_updated_dfapi.show()
-
-    # ------------ spark sql method ------------
-
-    # cached_df_nasa_feed.createOrReplaceTempView("NASA_FEED")
-
-    # df_nasa_feed_updated_sparksql = spark.sql("""
-    # SELECT *, ROW_NUMBER() OVER(PARTITION BY date ORDER BY neo_reference_id) AS row_number
-    # FROM NASA_FEED
-    # """)
-
-    # df_nasa_feed_updated_sparksql.printSchema()
-    # df_nasa_feed_updated_sparksql.show()
-
-    df_nasa_feed_updated_dfapi.write.mode('overwrite').partitionBy(
-        "date").parquet(cfg.absolute_paths["parquet_abs_path"])
-
-    df_nasa_feed.unpersist()  # we don't need it anymore
+        raise AirflowException({e})
 
 
-def load_parquet_to_mongodb_stage():
+def transform_and_write_to_parquet():
+
+    try:
+
+        global spark
+
+        nasa_neo_df = spark.read.option("multiline", "true").json(
+            cfg.absolute_paths["json_abs_path"])
+
+        # df_nasa_feed.printSchema()
+        # df_nasa_feed.show()
+
+        cached_nasa_neo_df = nasa_neo_df.cache()
+
+        windowSpec = Window.partitionBy("date").orderBy("neo_reference_id")
+
+        df_nasa_feed_updated_dfapi = cached_nasa_neo_df.withColumn(
+            "row_number", row_number().over(windowSpec))
+
+        df_nasa_feed_updated_dfapi.write.mode('overwrite').partitionBy(
+            "date").parquet(cfg.absolute_paths["parquet_abs_path"])
+
+        nasa_neo_df.unpersist()  # we don't need it anymore
+
+    except Exception as e:
+
+        raise AirflowException({e})
+
+
+def load_parquet_to_mongodb_staging():
     global spark
 
     nasa_feed_df = spark.read.parquet(cfg.absolute_paths["parquet_abs_path"])
@@ -156,29 +152,32 @@ def load_parquet_to_mongodb_stage():
         .option("collection", cfg.mongo_db["staging_collection"]) \
         .save()
 
-    # spark.stop()
 
+def populate_mongodb_production():
 
-def update_mongodb_production():
-    with MongoClient(host=cfg.mongo_db["host"], port=cfg.mongo_db["port"]) as client:
+    try:
+        with MongoClient(host=cfg.mongo_db["host"], port=cfg.mongo_db["port"]) as client:
 
-        database = getattr(client, cfg.mongo_db["database"])
-        staging_collection = getattr(
-            database, cfg.mongo_db["staging_collection"])
-        # equilevant client.nasa_gov.nasa_neo_service_production
-        production_collection = getattr(
-            database, cfg.mongo_db["production_collection"])
+            database = getattr(client, cfg.mongo_db["database"])
+            staging_collection = getattr(
+                database, cfg.mongo_db["staging_collection"]) # equilevant client.nasa_gov.nasa_neo_service_production ffff
+            production_collection = getattr(
+                database, cfg.mongo_db["production_collection"])
 
-        staging_documents = []
+            staging_documents = []
 
-        # create dict list with stage rows
-        for doc in staging_collection.find():
-            staging_documents.append(doc)
+            # create dict list with stage rows
+            for doc in staging_collection.find():
+                staging_documents.append(doc)
 
-        # delete from prod rows that exist on stage
-        for row in staging_documents:
-            production_collection.delete_one(
-                {"date": row["date"], "neo_reference_id": row["neo_reference_id"]})
+            # delete from prod rows that exist on stage
+            for row in staging_documents:
+                production_collection.delete_one(
+                    {"date": row["date"], "neo_reference_id": row["neo_reference_id"]})
 
-        # load stage records to prod
-        production_collection.insert_many(staging_documents)
+            # load stage records to prod
+            production_collection.insert_many(staging_documents)
+
+    except Exception as e:
+
+        raise AirflowException({e})
--- jobs/nasa_neo_service_dag.py
+++ jobs/nasa_neo_service_dag.py
@@ -15,11 +15,12 @@ default_args = {
     "owner": "airflow",
     "depends_on_past": False,
     "start_date": datetime(2019, 4, 30),
-    "email": ["kokargag@gmail.com", "konskar93@gmail.com"],
+    "email": cfg.smtp["receiver_email_list"], # ["kokargag@gmail.com", "konskar93@gmail.com"],
     "email_on_failure": True,
     "email_on_retry": False,
     "retries": 3,
-    "retry_delay": timedelta(minutes=1),
+    "retry_delay": timedelta(seconds=30),    
+    # "retry_delay": timedelta(minutes=1),
 }
 
 with DAG(
@@ -29,34 +30,32 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    get_nasa_feed_api_data = PythonOperator(
-        task_id="get_nasa_feed_api_data",
-        python_callable=f.get_nasa_feed_api_data,
-        retries=3
+    collect_api_data = PythonOperator(
+        task_id="collect_api_data",
+        python_callable=f.collect_api_data
     )
 
-    make_nasa_feed_parquet_file = PythonOperator(
-        task_id="make_nasa_feed_parquet_file",
-        python_callable=f.make_nasa_feed_parquet_file,
-        retries=3
+    transform_and_write_to_parquet = PythonOperator(
+        task_id="transform_and_write_to_parquet",
+        python_callable=f.transform_and_write_to_parquet
     )
 
-    load_parquet_to_mongodb_stage = PythonOperator(
+    load_parquet_to_mongodb_staging = PythonOperator(
         task_id="load_parquet_to_mongodb_stage",
-        python_callable=f.load_parquet_to_mongodb_stage,
+        python_callable=f.load_parquet_to_mongodb_staging,
         retries=3
     )
 
-    update_mongodb_production = PythonOperator(
-        task_id="update_mongodb_production",
-        python_callable=f.update_mongodb_production,
-        retries=3
+    populate_mongodb_production = PythonOperator(
+        task_id="populate_mongodb_production",
+        python_callable=f.populate_mongodb_production,
+        retries=1
     )
-    send_run_success_notification = EmailOperator(
-        task_id ='send_run_success_notification',
-        to = ["kokargag@gmail.com", "konskar93@gmail.com"],
-        subject = 'Airflow Notification: "nasa_neo_service_ingestion_dag"',
-        html_content = """ <h3>Dag run sucesfully</h3> """
-    )
+    # send_run_success_notification = EmailOperator(
+    #     task_id ='send_run_success_notification',
+    #     to = cfg.smtp["receiver_email_list"],
+    #     subject = 'Airflow Notification: "nasa_neo_service_ingestion_dag"',
+    #     html_content = """ <h3>Dag run sucesfully</h3> """
+    # )
 
-get_nasa_feed_api_data >> make_nasa_feed_parquet_file >> load_parquet_to_mongodb_stage >> update_mongodb_production >> send_run_success_notification
+collect_api_data >> transform_and_write_to_parquet >> load_parquet_to_mongodb_staging >> populate_mongodb_production # >> send_run_success_notification
